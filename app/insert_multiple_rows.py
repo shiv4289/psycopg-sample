2d7475ea7af72cf81e456e8e052d7eeba68fc089
Shiv <shiv4289@gmail.com>
2019-04-09 00:59:13 +0530

    Added sample codes

---
+++ app/insert_multiple_rows.py
@@ -0,0 +1,25 @@
+import psycopg2
+def bulkInsert(records):
+    try:
+        connection = psycopg2.connect(user="postgres",
+                                      # password="secret",
+                                      host="127.0.0.1",
+                                      port="5432",
+                                      database="postgres_db")
+        cursor = connection.cursor()
+        sql_insert_query = """ INSERT INTO mobile (id, model, price) 
+                           VALUES (%s,%s,%s) """
+        # executemany() to insert multiple rows rows
+        result = cursor.executemany(sql_insert_query, records)
+        connection.commit()
+        print(cursor.rowcount, "Record inserted successfully into mobile table")
+    except (Exception, psycopg2.Error) as error:
+        print("Failed inserting record into mobile table {}".format(error))
+    finally:
+        # closing database connection.
+        if (connection):
+            cursor.close()
+            connection.close()
+            print("PostgreSQL connection is closed")
+records_to_insert = [ (4,'LG', 800) , (2,'One Plus 6', 920)]
+bulkInsert(records_to_insert)
